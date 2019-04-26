3a9783b23bf4857f926b9328811157c776b31286
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-04-26 16:36:29 +0800

    测试

---
+++ 测试.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/4/26 16:23
+# @Author  : gitproject
+# @File    : 测试.py
+# @Software: PyCharm
+
+
+print("hello world python")
+print("hello world python")
+print("hello world python")
+print("hello world python")
+print("hello world python")
