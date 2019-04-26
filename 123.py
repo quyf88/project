793a2c79ba32e6a850c3b137ffd9987f0a20ee2d
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-04-26 16:44:48 +0800

    123

---
+++ 123.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/4/26 16:43
+# @Author  : project
+# @File    : 123.py
+# @Software: PyCharm
+
+
+print("hello world python")
+print("hello world python")
+print("hello world python")
+print("hello world python")
+print("hello world python")
+print("hello world python")
