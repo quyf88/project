a9f40524d6e1d31b60fd9bcf4acfc4bea262e371
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-06-03 00:42:57 +0800

    时间-日志模块

--- 潭州课堂/数据库/datatime_log_domo.py
+++ 潭州课堂/数据库/datatime_log_domo.py
@@ -12,31 +12,32 @@ from datetime import datetime
 2.使用logging模块化组件实现能记录错误信息到文件的程序，并在程序里制造错误，看错误信息是否被记录下来
 """
 
+class A:
 
-def my_logging():
-    m_log = logging.Logger('错误日志')
-    mh = logging.FileHandler('m_log.log')
-    mh.setLevel(logging.INFO)
-    fmt = logging.Formatter('时间：%(asctime)s 文件名：%(filename)s 行号%(lineno)d')
+    def my_logging(self):
+        m_log = logging.Logger('错误日志')
+        mh = logging.FileHandler('m_log.log')
+        mh.setLevel(logging.INFO)
+        fmt = logging.Formatter('时间：%(asctime)s 文件名：%(filename)s 行号%(lineno)d')
 
-    mh.setFormatter(fmt)
-    m_log.addHandler(mh)
+        mh.setFormatter(fmt)
+        m_log.addHandler(mh)
 
-    return m_log
+        return m_log
 
+    def life_long(self, birthday):
 
-def life_long(birthday):
-
-    try:
-        now = datetime.now()
-        end = datetime.strptime(birthday, '%Y-%m-%d')
-        days = now - end
-        return days.days
-    except Exception as e:
-        log = my_logging()
-        log.error(e)
-        # 输出详细错误信息
-        log.exception(e)
+        try:
+            now = datetime.now()
+            end = datetime.strptime(birthday, '%Y-%m-%d')
+            days = now - end
+            return days.days
+        except Exception as e:
+            self.my_logging().exception()
+            log = self.my_logging()
+            log.error(e)
+            # 输出详细错误信息
+            log.exception(e)
 
 
 if __name__ == '__main__':
