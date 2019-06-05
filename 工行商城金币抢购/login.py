14c97c4cb0f4a2a2087047679f2a8b1963463151
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-06-05 16:58:37 +0800

    细节优化

--- 工行商城金币抢购/login.py
+++ 工行商城金币抢购/login.py
@@ -4,10 +4,12 @@
 # IED    ：PyCharm
 # 创建时间 ：2019/6/2 2:06
 
-
 from selenium import webdriver
 
+# 启动360浏览器
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = r"C:\Users\Administrator\AppData\Roaming\360se6\Application\360se.exe"
+chrome_options.add_argument(r'--lang=zh-CN')  # 启动参数
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get('https://login.mall.icbc.com.cn/login?service=https%3A%2F%2Fmall.icbc.com.cn%2Fj_spring_cas_security_check')
 
-driver = webdriver.Chrome("C:\Program Files (x86)\Chrome\chromedriver.exe")
-driver.get('http://www.baidu.com')
-print(1)
