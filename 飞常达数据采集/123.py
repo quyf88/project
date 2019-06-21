2302d657ebdf3a0b2b0796a79a9b5b8b2effc436
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-06-21 19:08:57 +0800

    航班信息监控

--- 飞常达数据采集/123.py
+++ 飞常达数据采集/123.py
@@ -7,14 +7,5 @@
 import os
 import re
 q = '182108'
-path = os.path.abspath('.') + '\log\send_sms.txt'
-with open(path, 'a+', encoding='utf-8')as f:
-    f.seek(0)
-    a = [i.replace('\n', '') for i in f.readlines()]
-    if q in a:
-        print(1)
-    else:
-        f.write(q+'\n')
-
 
 
