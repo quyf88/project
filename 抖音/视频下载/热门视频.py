b9968927b45f35b020dd3d0cf8d35a6675da2bea
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-07-23 16:52:31 +0800

    抖音视频下载

--- 抖音/视频下载/热门视频.py
+++ 抖音/视频下载/热门视频.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/7/22 15:48
 # @Author  : project
-# @File    : test.py
+# @File    : 热门视频.py
 # @Software: PyCharm
 
 
@@ -37,8 +37,8 @@ def postHTML(url):
     :param url:
     :return:
     '''
+
     rsp = requests.post(url, headers=headers, verify=False)
-    print(rsp.content.decode(rsp.apparent_encoding, 'ignore'))
     return rsp.content.decode(rsp.apparent_encoding, 'ignore')
 
 
@@ -51,14 +51,14 @@ def getVideo(key):
     # 编译关键词
     key = urllib.parse.quote(key)
     # 拼接关键词搜索接口url
-    # hot_search 0普通搜索 1热门搜索
-    # url = 'https://api.amemv.com/aweme/v1/general/search/single/?keyword=' + key + '&offset=0&count=10&is_pull_refresh=0&hot_search=0&latitude=30.725991&longitude=103.968091&ts=1543984658&js_sdk_version=1.2.2&app_type=normal&manifest_version_code=350&_rticket=1543984657736&ac=wifi&device_id=60155513971&iid=53112482656&os_version=8.0.0&channel=xiaomi&version_code=350&device_type=MI%205&language=zh&uuid=862258031596696&resolution=1080*1920&openudid=8aa8e21fca47053b&update_version_code=3502&app_name=aweme&version_name=3.5.0&os_api=26&device_brand=Xiaomi&ssmix=a&device_platform=android&dpi=480&aid=1128&as=a1e5055072614ce6a74033&cp=5813c65d2e7d0769e1[eIi&mas=01327dcd31044d72007555ed00c3de0b5dcccc0c2cec866ca6c62c'
-    url = 'https://aweme-hl.snssdk.com/aweme/v1/discover/search/?cursor=0&keyword=' + key +'&count=10&type=1&is_pull_refresh=1&hot_search=0&ts=1563785163&js_sdk_version=1.13.10&app_type=normal&os_api=25&device_platform=android&device_type=OS105&iid=72586330332&ssmix=a&manifest_version_code=590&dpi=400&uuid=86795503044051&version_code=590&app_name=aweme&version_name=5.9.0&openudid=c2f22121e41a5d07&device_id=50751127063&resolution=1080*2070&os_version=7.1.1&language=zh&device_brand=SMARTISAN&ac=wifi&update_version_code=5902&aid=1128&channel=smartisan&_rticket=1563785162163&mcc_mnc=46000&as=a125c7e3ab0ced87754277&cp=79cedb5eb4553a7ee1OaWe&mas=011c8e49f4af7b5cbff404b102685f2d16ecec4c2cacecec1c26a6'
-    print(url)
+    # hot_search 0普通搜索 1热门搜索 type=1 用户列表
+    url = 'https://aweme-hl.snssdk.com/aweme/v1/general/search/single/?keyword=' + key + '&offset=0&count=10&is_pull_refresh=0&hot_search=0&latitude=30.725991&longitude=103.968091&ts=1543984658&js_sdk_version=1.2.2&app_type=normal&manifest_version_code=350&_rticket=1543984657736&ac=wifi&device_id=60155513971&iid=53112482656&os_version=8.0.0&channel=xiaomi&version_code=350&device_type=MI%205&language=zh&uuid=862258031596696&resolution=1080*1920&openudid=8aa8e21fca47053b&update_version_code=3502&app_name=aweme&version_name=3.5.0&os_api=26&device_brand=Xiaomi&ssmix=a&device_platform=android&dpi=480&aid=1128&as=a1e5055072614ce6a74033&cp=5813c65d2e7d0769e1[eIi&mas=01327dcd31044d72007555ed00c3de0b5dcccc0c2cec866ca6c62c'
+    # url = 'https://aweme-hl.snssdk.com/aweme/v1/discover/search/?keyword=' + key + '&offset=0&count=10&type=1&is_pull_refresh=0&hot_search=0&latitude=30.725991&longitude=103.968091&ts=1543984658&js_sdk_version=1.2.2&app_type=normal&manifest_version_code=350&_rticket=1543984657736&ac=wifi&device_id=60155513971&iid=53112482656&os_version=8.0.0&channel=xiaomi&version_code=350&device_type=MI%205&language=zh&uuid=862258031596696&resolution=1080*1920&openudid=8aa8e21fca47053b&update_version_code=3502&app_name=aweme&version_name=3.5.0&os_api=26&device_brand=Xiaomi&ssmix=a&device_platform=android&dpi=480&aid=1128&as=a1e5055072614ce6a74033&cp=5813c65d2e7d0769e1[eIi&mas=01327dcd31044d72007555ed00c3de0b5dcccc0c2cec866ca6c62c'
     # 获取搜索界面并转化为json对象
     jsonObj = json.loads(postHTML(url))
     # 获取data对应v
-    metes = jsonObj['data']
+    print(jsonObj)
+    metes = jsonObj['user_list']
     nums = len(metes)
     uri = ''
     # 多个视频列表捕获第一个视频地址即刻返回视频uri(视频唯一标识)
