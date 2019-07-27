a4622fabf5d440c43cee58c1540517145488eeec
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-07-27 21:00:51 +0800

    time

---
+++ 基类模块/模块常用语法/time.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+# 作者    ： Administrator
+# 文件    ：time.py
+# IED    ：PyCharm
+# 创建时间 ：2019/7/27 20:29
+
+import time
+import datetime
+
+t = time.time()
+
+print(t)                       #原始时间数据
+print(int(t))                  #秒级时间戳
+print(int(round(t * 1000)))    #毫秒级时间戳
+
+nowTime = lambda:int(round(t * 1000))
+print(nowTime())             #毫秒级时间戳，基于lambda
+
+print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
