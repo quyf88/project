ecd84e085e9f623bf0f17c0beb869fa111686acc
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-08-26 13:11:26 +0800

    co

--- 基类模块/模块常用语法/selenium常用语法.py
+++ 基类模块/模块常用语法/selenium常用语法.py
@@ -35,13 +35,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
-# 设置代理
-chrome_options.add_argument("--proxy-server=https://183.129.207.92:18625")
-# 一定要注意，=两边不能有空格，不能是这样--proxy-server = http://202.20.16.82:10152
 driver = webdriver.Chrome(chrome_options=chrome_options, keep_alive=False)
-# 查看本机ip，查看代理是否起作用
-driver.get("http://httpbin.org/ip")
-print(driver.page_source)
 wait = WebDriverWait(driver, 8, 0.5)  # 设置隐式等待时间
 driver.maximize_window()  # 窗口最大化
 
@@ -102,3 +96,48 @@ driver.get('https://www.v99two.com/')
 # driver请求添加cookie参数
 cookies = {'domain': '.v99two.com', 'name': 'PHPSESSID', 'value': '6cc7af233de034dd73d6333a2db5e5d8'}
 driver.add_cookie(cookie_dict=cookies)
+
+# 设置代理
+class Proxy(object):
+    def __init__(self):
+        proxies = requests.get('http://www.uu-ip.com/ProxyiPAPI.aspx?action=GetIPAPI&qty=1&ordernumber=56b059a66c25a48db7a3e4426ff6573c').text
+        self.proxy_ip = 'http://{}'.format(proxies)
+        print(self.proxy_ip)
+        self.browser = self.getbrowser()
+        self.getpage(self.browser)
+
+    def getbrowser(self):
+        options = webdriver.ChromeOptions()
+        # 设置代理
+        desired_capabilities = webdriver.DesiredCapabilities.INTERNETEXPLORER.copy()
+        desired_capabilities['proxy'] = {
+            "httpProxy": self.proxy_ip,
+            "proxyType": "MANUAL",  # 此项不可注释
+            # "ftpProxy": self.proxy_ip,  # 代理ip是否支持这个协议
+            # "sslProxy": self.proxy_ip,  # 代理ip是否支持这个协议
+            # "noProxy": None,
+            # "class": "org.openqa.selenium.Proxy",
+            # "autodetect": False
+        }
+        # 使用无头模式
+        options.add_argument('headless')
+        browser = webdriver.Chrome(chrome_options=options,
+                                   desired_capabilities=desired_capabilities)
+        return browser
+
+    def getpage(self, browser):
+        # 打开目标网站
+        browser.get("https://www.baidu.com")
+        # 对整个页面进行截图
+        browser.save_screenshot('百度.png')
+        # 打印网站的title信息
+        print(browser.title)
+
+        # 检测代理ip是否生效
+        browser.get("http://httpbin.org/ip")
+        # 获取当前所有窗口集合(list类型) --- 因为打开多个窗口
+        handles = browser.window_handles
+        # 切换到最新的窗口
+        browser.switch_to_window(handles[-1])
+        # 打印新窗口网页的内容
+        print(browser.page_source)
