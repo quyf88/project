b1c26baf341d3672425d4eaefb037a28b7958c2f
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-09-06 16:58:45 +0800

    xia

--- 小爬虫/小项目/comic/123.py
+++ 小爬虫/小项目/comic/123.py
@@ -8,40 +8,56 @@ import re
 
 
 b = """
+ 
         <table style="width:100%;">
         <tbody><tr>
-            <td width="126" align="center" style="padding-top:0px;white-space:nowrap;" onclick="episode_click('7429','cd597b9d386e085f51bb7e6ad963e949', '6502521');">
-                <div style="float:center;position:relative;width:126px; height:68px; margin:9px;    background-size:cover;background-position:center 20%;border-radius:0px;background-image:url('//ballerina.toptoon.net/assets/upfile/ep_thumb/x2/330_7429_1469687571.9003.jpg');" class="comic_cell">&nbsp;</div>
+            <td align="center" style="text-align: left;width:15%;min-height:180px;max-width:210px;padding-top:0px;white-space:nowrap;" onclick="episode_click('27235','54882c34332256fc6432a93eb7c0ad24', '6396591');">
+                <div style="background:url('//ballerina.toptoon.net/assets/upfile/ep_thumb2/x2/1015_27235_1558419505.2609.jpg');background-size:cover;min-height:168px;width:94%;margin:5% 3% 5% 5%;border-radius:5px;"></div>
             </td>
-            <td style="padding:7px 5px 0px 5px;line-height:19px;height:25px;">
-                <div style="float:left; position:absolute; top:15px;left:148px; width:calc(100% - 155px);" onclick="episode_click('7429','cd597b9d386e085f51bb7e6ad963e949', '6502521');">
+            <td align="center" style="text-align: left;width:15%;min-height:180px;max-width:210px;padding-top:0px;white-space:nowrap;" onclick="episode_click('27235','54882c34332256fc6432a93eb7c0ad24', '6396591');">
+                <div style="background:url('//ballerina.toptoon.net/assets/upfile/ep_thumb4/x2/1015_27235_1558419505.265.jpg');background-size:cover;min-height:168px;width:94%;margin:5% 5% 5% 3%;border-radius:5px;"></div>
+            </td>
+            <td style="padding:17px 15px 10px 15px;min-height:130px;line-height:19px;height:25px;box-sizing: border-box;vertical-align: top;">
+                <div style="float:right; width: 67%; min-height:95px; max-height: 264px; border: 1px solid rgba(0,0,0,0.1);padding:35px 24px;;border-radius:5px;box-sizing: border-box;position: absolute;height: 80%;" onclick="episode_click('27235','54882c34332256fc6432a93eb7c0ad24', '6396591');">
                     
-            <span style="font-size:16px; font-weight:600; color:#000;">第3話</span>
+            <span style="font-size:16px; font-weight:600; color:#000;">第1話</span>
                                                     <br>
-                                                    <span style="color:#999; font-size:11px;" onclick="episode_click('7429','cd597b9d386e085f51bb7e6ad963e949', '6502521');">
-                                                    2016.08.02																				</span>
+                                                    <span style="color:#999; font-size:11px;" onclick="episode_click('27235','54882c34332256fc6432a93eb7c0ad24', '6396591');">
+                                                    2019.06.03																				</span>
             <br>																			</div>
 
 
         
-        <div style="right:33px; position:absolute; top:15px;" class="vote_list" onclick="episode_click('7429','cd597b9d386e085f51bb7e6ad963e949', '6502521');">
-                                                    <div style="line-height:20px;margin:0px 0px;">
-                <div style="background-color:#fff;border:1px solid #e33;padding:14px;border-radius:4px;font-size:14px;font-weight:bold;color:#e33;cursor:pointer;line-height:12px;text-align:center;white-space:nowrap;">
-                
-                <span style="text-decoration: line-through;">&nbsp;2點券&nbsp;</span>
-                <br>
-                &nbsp;免費&nbsp;</div>
-                </div>
-                                                </div>
+        <div style="right:33px; position:absolute; top:84px;" class="vote_list" onclick="episode_click('27235','54882c34332256fc6432a93eb7c0ad24', '6396591');">
+        <span style="background-color:#fff;border:1px solid #e33;padding:14px;border-radius:4px;font-size:14px;font-weight:bold;color:#e33;cursor:pointer;line-height:12px;white-space:nowrap;">&nbsp;免費&nbsp;</span>									</div>
 
     </td>
 </tr>
 </tbody></table>
 """
+#
+# id = re.findall(r'episode_click(.*?),', b, re.S | re.M)
+# chapter = re.findall(r'第(.*?)話', b)
+# coupon = re.findall(r'&nbsp;(.*?)&nbsp;</span>', b)
+# print(id)
+# print(chapter)
+# print(coupon)
+import string
+# coupon = re.findall(r'&nbsp;(.*?)&nbsp;</span>', b)
+# print(coupon)
 
-id = re.findall(r'episode_click(.*?),', b, re.S | re.M)
-chapter = re.findall(r'第(.*?)話', b)
-coupon = re.findall(r'&nbsp;(.*?)&nbsp;</span>', b)
-print(id)
-print(chapter)
-print(coupon)
+with open('2019-09-06更新记录.txt', 'a+', encoding='utf-8') as f:
+    f.seek(0)
+    # print(f.readlines())
+    readlin = [i.strip() for i in f.readlines()]
+    print(readlin)
+    # print(readlin)
+    if '人夫的悸' not in readlin:
+        print(1)
+    else:
+        print(2)
+# c = ['免費']
+# if ('免費' not in c) and ('收藏中' not in c):
+#     print(1)
+# else:
+#     print(2)
--- 小爬虫/小项目/comic/spider.py
+++ 小爬虫/小项目/comic/spider.py
@@ -45,12 +45,16 @@ class Spider:
         self.driver = webdriver.Chrome(chrome_options=options, desired_capabilities=desired_capabilities)
         self.wait = WebDriverWait(self.driver, 40, 1)  # 设置隐式等待时间
         self.driver.maximize_window()  # 窗口最大化
-
-        self.path_name = None
+        # 当前漫画文件夹路径
+        self.comic_path = None
+        # 更新的章节数
         self.num = None
+        # 章节路径
+        self.chapter_path = None
 
     def login(self):
         count = 1
+        print('*'*20, '账号登录中', '*'*20)
         while True:
             url = 'https://www.toptoon.net'
             self.driver.get(url)
@@ -60,9 +64,9 @@ class Spider:
             login.click()
             # 账号密码
             user_id = self.wait.until(EC.presence_of_element_located((By.ID, 'user_id')))
-            user_id.send_keys('1033383881@qq.com')
+            user_id.send_keys('dspwb20190808@outlook.com')
             user_pw = self.driver.find_element_by_id('user_pw')
-            user_pw.send_keys('chenxiaoli2013')
+            user_pw.send_keys('Ddqqw333e123@#!!U')
             self.driver.find_element_by_xpath('//*[@id="alert_data"]/div[6]/span').click()
             if count > 3:
                 print('账号登录失败,联系开发者!')
@@ -95,10 +99,8 @@ class Spider:
         """
         self.driver.get(url)
         # 总章数
-        total = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="page"]/table[2]/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/td[1]/div/div')))
-        print(len(total))
-        total = str(len(total) - 1)
-        print('总章数：{}'.format(total))
+        total = self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.filtr-item')))
+        print('总章数：{}'.format(len(total)))
         # 文章名
         article_name = self.driver.find_element_by_css_selector('#tab1_board > div > div:nth-child(1)').text
         print('文章名：{}'.format(article_name))
@@ -106,7 +108,7 @@ class Spider:
         summary = self.driver.find_element_by_xpath('//*[@id="tab1_board"]/div/div[2]').text
         summary = summary.replace('\n', '').replace(' ', '')
         print('文章简介：{}'.format(summary))
-        data = [url, article_name, total, summary]
+        data = [url, article_name, str(len(total)), summary]
         return data
 
     def validation(self, data):
@@ -118,7 +120,7 @@ class Spider:
         # 写入文件
         path = 'image/' + str(article_name)
         path_name = path + '/' + article_name + '.txt'
-        self.path_name = path + '/' + article_name
+        self.comic_path = path
         if not os.path.exists(path):
             os.mkdir(path)
             self.num = int(total)
@@ -151,8 +153,8 @@ class Spider:
         filename = date + '更新记录.txt'
         with open(filename, 'a+', encoding='utf-8') as f:
             f.seek(0)
-            print(article_name, f.read())
-            if article_name not in f.readlines():
+            readlin = [i.strip() for i in f.readlines()]
+            if article_name not in readlin:
                 f.write(article_name)
                 f.write('\n')
 
@@ -161,45 +163,94 @@ class Spider:
         获取章节编号拼接详情页面url
         :return: 详情url
         """
-        details = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="page"]/table[2]/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/td[1]/div/div')))
+        details = self.driver.find_elements_by_css_selector('.filtr-item')
+        # 翻转列表
+        details = list(reversed(details))
         details_url = []
-        for i in range(1, self.num+1):
+        for i in range(self.num+1):
             # 获取章节标签源码
             html = details[i].get_attribute('innerHTML')
             # print(html)
-            # 提取章节ID 拼接详情url
-            id = re.findall(r'episode_click(.*?),', html, re.S | re.M)
-            # 提取章节
-            chapter = re.findall(r'第(.*?)話', html)
+            # 判断是否免费
             # 提取金额
             coupon = re.findall(r'&nbsp;(.*?)&nbsp;</span>', html)
+            print(coupon)
+            if ('免費' not in coupon) and ('收藏中' not in coupon):
+                # 滑动滚动条到某个指定的元素
+                js4 = "arguments[0].scrollIntoView();"
+                # 将下拉滑动条滑动到当前div区域
+                self.driver.execute_script(js4, details[i])
+
+                # 购买章节
+                if not self.buy_comics(details[i]):
+                    print('当前章节购买失败!')
+                    continue
+
+            # 提取章节ID 拼接详情url
+            id = re.findall(r'episode_click(.*?),', html, re.S | re.M)
             if not id:
                 continue
             id = ''.join(re.findall(r'(\d)', id[0]))
-
+            # 第几章
+            chapter = re.findall(r'第(.*?)話', html)
+            print('第{}話'.format(chapter[0]))
+            if chapter:
+                self.chapter_path = self.comic_path + '/' + '第' + str(chapter[0]) + '話'
+                print(self.chapter_path)
             url = 'https://www.toptoon.net/comic/episode_view?episode_idx=' + str(id)
             details_url.append(url)
         print(details_url)
         return details_url
 
-    @retry(stop_max_attempt_number=3)
-    def get_extract_all_url(self, url):
+    def buy_comics(self, detail):
         """
-        提取图片真实url
+        购买章节
+        detail 购买的章节对象
         :return:
         """
-        print('提取详情url...')
-        # url = 'https://www.toptoon.net/#contents_80268,7421'
-        self.driver.get(url)
-        response = self.driver.page_source
-        print(response)
-        return
-        tree = etree.HTML(response)
-        div = tree.xpath('//*[@id="viewer_data"]/@src')[0]
-        _url = 'https://www.toptoon.net/' + div
-        print('成功提取详情url:{}'.format(div))
-        return _url
+        count = 1
+        while True:
+            # 点击关闭
+            close = self.driver.find_elements_by_css_selector('#close')
+            if close:
+                close[0].click()
+            # 点击弹出购买窗口
+            detail.click()
+            # 判断窗口是否弹出
+            buys = WebDriverWait(self.driver, 10, 1, AttributeError).until(
+                        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="alert_data"]/div[3]/table/tbody/tr')))
+            if not buys:
+                if count >= 3:
+                    print('购买失败!')
+                    return False
+                count += 1
+                continue
+            # 点击购买当前章节
+            buys[0].click()
+            # 确认购买
+            enter = self.driver.find_element_by_css_selector('#p_btn_1')
+            print(enter.text)
+            if enter.text == '確認':
+                enter.click()
+                return True
 
+    @retry(stop_max_attempt_number=3)
+    # def get_extract_all_url(self, url):
+    #     """
+    #     提取图片真实url
+    #     :return:
+    #     """
+    #     print('提取详情url...')
+    #     # url = 'https://www.toptoon.net/#contents_80268,7421'
+    #     self.driver.get(url)
+    #     response = self.driver.page_source
+    #     print(response)
+    #     return
+    #     tree = etree.HTML(response)
+    #     div = tree.xpath('//*[@id="viewer_data"]/@src')[0]
+    #     _url = 'https://www.toptoon.net/' + div
+    #     print('成功提取详情url:{}'.format(div))
+    #     return _url
     @retry(stop_max_attempt_number=3)
     def get_image_url(self, url):
         """
@@ -219,7 +270,6 @@ class Spider:
         for i in image_url_list:
             image_url = i.xpath('./img/@src')[0]
             url_list.append(image_url)
-
         return url_list
 
     @retry(stop_max_attempt_number=3)
@@ -228,12 +278,14 @@ class Spider:
         下载图片
         :return:
         """
+        if not os.path.exists(self.comic_path):
+            os.mkdir(self.comic_path)
         for i in range(len(url_list)):
             url = 'http:' + url_list[i]
             # print(url)
             response = requests.get(url, headers=self.headers, proxies=self.proxies, timeout=10)
             # print(response.status_code)
-            image_path = self.path_name + '/' + str(i) + '.png'
+            image_path = self.comic_path + '/' + str(i) + '.png'
             with open(image_path, 'wb') as image_content:
                 image_content.write(response.content)
                 print('图片 {} 保存成功!'.format(i+1))
