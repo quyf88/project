dad0ecd8f42fed7fa7a448ff220fcf6443dbc3cd
Administrator <1033383881@qq.com>
2019-10-23 16:57:12 +0800

    twitter

--- 小爬虫/twitter/123.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-# 文件 ：123.py
-# IED ：PyCharm
-# 时间 ：2019/10/22 0022 9:51
-# 版本 ：V1.0
-a = ['New Generation Leaders', 'Javid Ahmad', 'Resolute Support', 'NATO', 'Javed Hamim Kakar', 'Khoshhal Taib', 'Daud Junbish', 'sayed salahuddin', 'Amrullah Saleh', 'Bashir Ahmad Gwakh', 'Dr. Abdullah Abdullah', 'Mohammed Haneef Atmar', 'Atlantic Council', 'Munich Security Conference', 'Jens Stoltenberg', 'Narendra Modi', 'ارگ', '']
-a = [i for i in a if i]
-print(a)
--- 小爬虫/twitter/spider.py
+++ 小爬虫/twitter/spider.py
@@ -4,13 +4,10 @@
 # @File    : spider.py
 # @Software: PyCharm
 import re
-import os
+import csv
 import time
-import logging
-import datetime
-import requests
-from lxml import etree
-from retrying import retry
+import pandas as pd
+from datetime import datetime
 from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -19,6 +16,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+def run_time(func):
+    def new_func(*args, **kwargs):
+        start_time = datetime.now()
+        print("程序开始时间：{}".format(start_time))
+        func(*args, **kwargs)
+        end_time = datetime.now()
+        print("程序结束时间：{}".format(end_time))
+        print("程序执行用时：{}s".format((end_time - start_time)))
+
+    return new_func
 
 
 class Spider:
@@ -49,57 +58,120 @@ class Spider:
         self.wait = WebDriverWait(self.driver, 5, 1)  # 设置隐式等待时间
         self.driver.maximize_window()  # 窗口最大化
 
-    def get_basic(self):
+        self.count = 1
+
+    def read_xls(self):
         """
-        获取基本信息: 发推数量、关注数、关注ID列表、粉丝数、
+        读取 表格数据
         :return:
         """
-        url = 'https://twitter.com/ashrafghani'
+        print('读取配置文件')
+        # 加载数据
+        df_read = pd.read_excel('config/用户id列表.xlsx')
+        df = pd.DataFrame(df_read)
+        # 获取指定表头的列数
+        ID = 0
+        for i in range(len(df.keys())):
+            if df.keys()[i] == 'ID':
+                ID = i
+        for indexs in df.index:
+            # 读取指定行列数据 df.ix[行,列]
+            data = df.ix[indexs, ID]
+            yield data
+
+    def get_basic(self, acc_id):
+        """
+        获取基本信息: 推文数量、关注数、关注ID列表、粉丝数
+        :return:
+        """
+        url = f'https://twitter.com/{acc_id}'
         self.driver.get(url)
         # 登录账号
         input('账号登录')
         # 发布文章数量
         Twitter_num = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[1]/div/div/div/div/div/div[2]/div/div'))).text
-        print(Twitter_num)
+        # print(Twitter_num)
         # 关注人数 关注列表页url
         attention_num = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/span[1]'))).text
-        print(attention_num)
+        # print(attention_num)
         # attention_url = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/@href')))
         attention_url = 'https://twitter.com/ashrafghani/following'
-        print(attention_url)
+        # print(attention_url)
         # 粉丝
         fan = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[2]/a/span[1]/span'))).text
-        print(fan)
-        print(f'账号：ashrafghani, 发布推特数量：{Twitter_num}, 关注人数：{attention_num}, 粉丝：{fan}')
+        # print(fan)
+        print(f'账号：{acc_id}, 发布推特数量：{Twitter_num}, 关注人数：{attention_num}, 粉丝：{fan}')
 
         # 获取关注ID列表
         self.driver.get(attention_url)
-
         time.sleep(3)
+        # 获取body的高度，滑到底部
+        scroll = "window.scrollTo(0,document.body.scrollHeight)"
+        self.driver.execute_script(scroll)
+
         attentions_id = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/section/div/div/div/div')))
-        attentions_id[0].send_keys(Keys.PAGE_DOWN)
         attentions_id = [i.text for i in attentions_id if i.text]
 
         attention_id = []
         for i in attentions_id:
             i = i.split('\n')
             attention_id.append(i[0])
-        print(f'关注列表：{attention_id}')
+        # print(f'关注列表：{attention_id}')
+        content = [acc_id, Twitter_num, attention_num, fan, attention_id]
+        return content
 
-    def get_teitter_content(self):
+    def get_teitter_content(self, Twitter_num):
         """
         获取推特文本内容及相应获赞数量，评论数量，转发数量
         :return:
         """
         url = 'https://twitter.com/ashrafghani'
         self.driver.get(url)
-        tweets = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div/div/div/article/div/div[2]/div[2]')))
-        print(len(tweets))
+        count = 1
 
-        tweets[0].sendKeys(Keys.PAGE_DOWN)
+        while True:
+            if count >= Twitter_num:
+                break
+            # 设置休眠时间 防止页面元素加载出错
+            time.sleep(2)
+            # 推文信息
+            tweets = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div/div/div/article/div/div[2]/div[2]')))
+            if not tweets:
+                continue
+            tweets = [i.text.split('\n') for i in tweets if i.text]
+            # print(tweets, len(tweets))  # 列表镶嵌列表
+            yield tweets
+            # 滚动屏幕
+            scroll = "window.scrollTo(0,document.body.scrollHeight)"
+            self.driver.execute_script(scroll)
 
+            count += 1
 
-        time.sleep(100)
+    def processing(self, tweets):
+        """
+        数据处理
+        tweets 列表镶嵌列表
+        :return:
+        """
+        for tweet in tweets:
+            content = tweet[4]
+            comment = tweet[-3]
+            forward = tweet[-2]
+            like = tweet[-1]
+            yield content, comment, forward, like
+
+    def save_data(self, data):
+        """保存为csv"""
+        with open("twitter.csv", "a+", encoding='utf-8', newline="") as f:
+            k = csv.writer(f, delimiter=',')
+            with open("twitter.csv", "r", encoding='utf-8', newline="") as f1:
+                reader = csv.reader(f1)
+                if not [row for row in reader]:
+                    k.writerow(['ID', '推文数量', '关注人数', '关注列表', '粉丝数', '推文内容', '评论数', '转发数', '点赞数'])
+                    k.writerows(data)
+                else:
+                    k.writerows(data)
+            print(f'第：{self.count}条数据插入成功!')
 
     def quit(self):
         """
@@ -108,9 +180,24 @@ class Spider:
         """
         self.driver.quit()
 
+    def run(self):
+        for acc_id in self.read_xls():
+            # 获取个人基本信息
+            acc, Twitter_num, attention_num, fan, attention_id = self.get_basic(acc_id)
+            # 推文数量
+            Twitter_num = int(''.join(re.findall(r'\d', Twitter_num)))
+            # 获取推文信息
+            for tweets in self.get_teitter_content(Twitter_num):
+                # 提取信息
+                for contents in self.processing(tweets):
+                    content, comment, forward, like = contents
+                    # 数据保存
+                    data = [[acc, Twitter_num, attention_num, attention_id, fan, content, comment, forward, like]]
+                    self.save_data(data)
+                    self.count += 1
+        self.quit()
+
 
 if __name__ == '__main__':
     spider = Spider()
-    spider.get_basic()
-    spider.get_teitter_content()
-    spider.quit()
+    spider.run()
