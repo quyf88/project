ed48a946093a144c68c59e83a9cc1e607533774a
Administrator <1033383881@qq.com>
2019-10-31 16:55:35 +0800

    抖音

--- 抖音/获取企业账号信息/123.py
+++ 抖音/获取企业账号信息/123.py
@@ -1,19 +1,31 @@
 # -*- coding:utf-8 -*-
 # 文件 ：123.py
 # IED ：PyCharm
-# 时间 ：2019/10/30 0030 16:37
+# 时间 ：2019/10/31 0031 13:25
 # 版本 ：V1.0
+from appium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.webdriver.support import expected_conditions as EC
+"""
+Appium adb 获取真实appActivity
+https://blog.csdn.net/qq_38154948/article/details/90408056
+"""
 
-import json
 
-
-def response(flow):
-    url = 'https://aweme-hl.snssdk.com/aweme/v1/discover/search'
-    # 筛选出以上面url为开头的url
-    print(123)
-    print('123')
-    if flow.request.url.startswith(url):
-        text = flow.response.text
-        # 将已编码的json字符串解码为python对象
-        data = json.loads(text)
-        print(data)
+class Spider:
+    def __init__(self):
+        self.desired_caps = {
+            "platformName": "Android",
+            "deviceName": "OS105",
+            "appPackage": "com.tencent.mm",
+            "appActivity": ".ui.LauncherUI",
+            'noReset': True  # 获取登录状态
+        }
+        self.driver_server = 'http://127.0.0.1:4723/wd/hub'
+        print('**********程序启动中**********')
+        # 启动微信
+        self.driver = webdriver.Remote(self.driver_server, self.desired_caps)
+        # 设置隐形等待时间
+        self.wait = WebDriverWait(self.driver, 10, 1, AttributeError)
---
+++ 抖音/获取企业账号信息/spider.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+# 文件 ：spider.py
+# IED ：PyCharm
+# 时间 ：2019/10/30 0030 16:37
+# 版本 ：V1.0
+"""
+切换至脚本目录下运行脚本
+mitmdump -s douyin_t.py
+"""
+
+
+import json
+
+
+def response(flow):
+    url = 'https://aweme-hl.snssdk.com/aweme/v2/comment/list/'
+
+    # 筛选出以上面url为开头的url
+    if flow.request.url.startswith(url):
+        # 获取评论json数据
+        text = flow.response.text
+        # 将已编码的json字符串解码为python对象
+        content = json.loads(text)
+        # 评论内容
+        comments = content['comments']
+        for comment in comments:
+            # 评论内容
+            text = comment['text']
+            # 用户名
+            nickname = comment['user']['nickname']
+            # 个性签名
+            signature = comment['user']['signature']
+            signature = signature.replace('\n', '').replace(' ', '')
+            if '微信' in signature or 'VX' in signature or 'vx' in signature or 'V' in signature or 'v' in signature:
+                print(f'用户名：{nickname} 个性签名：{signature} 评论内容：{text}')
+                # data = f'用户名：{nickname} 个性签名：{signature} 评论内容：{text}'
+                data = signature
+                save_txt(data)
+
+
+def save_txt(data):
+    with open('demo.txt', 'a+', encoding='utf-8') as f:
+        f.write(data)
+        f.write('\n')
+
