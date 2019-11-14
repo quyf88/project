eb451cd00de3f6f7e8de1bf843b9fa4160f5d933
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2019-11-14 23:41:56 +0800

    银行卡转账

--- 小爬虫/转账码生成/银行卡转账码/spider.py
+++ 小爬虫/转账码生成/银行卡转账码/spider.py
@@ -19,7 +19,7 @@
 
 短链接 转换 ：百度短链接
 查询银行代码
-https://ccdcapi.alipay.com/validateAndCacheCardInfo.json?cardNo=填写卡号&cardBinCheck=true
+https://ccdcapi.alipay.com/validateAndCacheCardInfo.json?cardNo=6230580000244202425&cardBinCheck=true
 
 alipays://platformapi/startapp?appId=09999988&actionType=toCard&sourceId=bill&cardNo=6217000030001234567&bankAccount=%E9%A9%AC%E4%BA%91&money=0.01&amount=0.01&bankMark=CCB&bankName=%E4%B8%AD%E5%9B%BD%E5%BB%BA%E8%AE%BE%E9%93%B6%E8%A1%8C
 参数：
@@ -147,15 +147,15 @@ class Spider:
     def run(self):
         # 隐藏卡号url
         # cardID 隐藏卡号 获取方法  https://zhuanlan.zhihu.com/p/65495172
-        # content = ['621700***0439', 'CCB', '毛科伟', '中国建设银行', '1911091516270451164']
-        # _url = f'https://www.alipay.com/?appId=09999988&actionType=toCard&sourceId=bill&cardNo={content[0]}&' \
-        #        f'bankAccount={content[2]}&money=&amount=&bankMark={content[1]}&bankName={content[3]}&cardIndex={content[4]}&cardNoHidden=true&cardChannel=HISTORY_CARD&orderSource=from'
+        content = ['623058***2425', 'SPABANK', '张万年', '中国平安银行', '1911141548378124597']
+        _url = f'https://www.alipay.com/?appId=09999988&actionType=toCard&sourceId=bill&cardNo={content[0]}&' \
+               f'bankAccount={content[2]}&money=&amount=&bankMark={content[1]}&bankName={content[3]}&cardIndex={content[4]}&cardNoHidden=true&cardChannel=HISTORY_CARD&orderSource=from'
         # print(_url)
         # 普通转账url
-        content = self.read_txt()
-        _url = 'https://www.alipay.com/?appId=09999988&actionType=toCard&sourceId=bill&cardNo={}&bankAccount={}&' \
-               'money=&amount=&bankMark={}&bankName={}&orderSource='.format(content[1], content[0], content[3], content[4])
-        print(_url)
+        # content = self.read_txt()
+        # _url = 'https://www.alipay.com/?appId=09999988&actionType=toCard&sourceId=bill&cardNo={}&bankAccount={}&' \
+        #        'money=&amount=&bankMark={}&bankName={}&orderSource='.format(content[1], content[0], content[3], content[4])
+        # print(_url)
         # 长链接转换短链接
         short_link = self.get_url(_url)
         # 短链接生成二维码
