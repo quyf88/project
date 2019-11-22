e3c670965efef4d73f05bd557969bc19ce0db360
Administrator <1033383881@qq.com>
2019-11-22 17:04:12 +0800

    抖音

--- django_web/Scripts/Django_api/API/views.py
+++ django_web/Scripts/Django_api/API/views.py
@@ -5,13 +5,13 @@ from django.http import HttpResponse
 
 
 def get_user(request):
-    # 读取数据库返回数据到HTML
-    # user_list = Area.objects.all()
-    # for user in user_list:
-    #     print(user.area)
+    """读取数据库返回数据到HTML"""
+    user_list = Area.objects.all()
+    for user in user_list:
+        print(user.area)
     # render方法可接收三个参数，一是request参数，二是待渲染的html模板文件,三是保存具体数据的字典参数(选填)。
-    # return render(request, 'index.html', {'user_list': user_list})
+    return render(request, 'index.html', {'user_list': user_list})
 
-    # 返回json数据
-    resp = {'errorcode': 100, 'detail': 'Get success'}
-    return HttpResponse(json.dumps(resp), content_type='application/json')
+    """返回json数据"""
+    # resp = {'errorcode': 100, 'detail': 'Get success'}
+    # return HttpResponse(json.dumps(resp), content_type='application/json')
