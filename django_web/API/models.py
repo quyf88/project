0b32a0cb9ab83678105752e8efd3fd32479dc11b
Administrator <1033383881@qq.com>
2019-11-28 16:48:40 +0800

    django

--- django_web/API/models.py
+++ django_web/API/models.py
@@ -2,43 +2,20 @@ from django.db import models
 
 
 class AntiFraud(models.Model):
+    # 账号ID
     id = models.CharField(db_column='ID', primary_key=True, max_length=255)  # Field name made lowercase.
+    # 业务名称
     business = models.CharField(db_column='Business', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    status = models.CharField(db_column='Status', max_length=5, blank=True, null=True)  # Field name made lowercase.
+    # 有效期
+    expiration = models.CharField(db_column='Expiration', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    # 账号状态
+    status = models.CharField(db_column='Status', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    # 更新时间
+    udeta = models.DateTimeField(db_column='Udeta', blank=True, null=True)  # Field name made lowercase.
+    # 创建时间
     datatime = models.DateTimeField(db_column='Datatime')  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'anti-fraud'
 
-
-# class Area(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     areaid = models.CharField(db_column='areaID', max_length=50, blank=True, null=True)  # Field name made lowercase.
-#     area = models.CharField(max_length=60, blank=True, null=True)
-#     father = models.CharField(max_length=6, blank=True, null=True)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'area'
-#
-#
-# class City(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     cityid = models.CharField(db_column='cityID', max_length=6, blank=True, null=True)  # Field name made lowercase.
-#     city = models.CharField(max_length=50, blank=True, null=True)
-#     father = models.CharField(max_length=6, blank=True, null=True)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'city'
-#
-#
-# class Povince(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     provinceid = models.CharField(db_column='provinceID', max_length=6, blank=True, null=True)  # Field name made lowercase.
-#     province = models.CharField(max_length=40, blank=True, null=True)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'povince'
--- django_web/API/views.py
+++ django_web/API/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import AntiFraud
+import time
 import json
+import datetime
+from .models import AntiFraud
 from django.http import HttpResponse
 
 
@@ -12,26 +14,63 @@ def get_user(request, course):
     # # render方法可接收三个参数，一是request参数，二是待渲染的html模板文件,三是保存具体数据的字典参数(选填)。
     # return render(request, 'index.html', {'user_list': user_list})
 
+    """读取数据库返回json数据"""
     # antis = AntiFraud.objects.all()
     # ants = AntiFraud.objects.filter(id='460a23180f3411ea9aec28d2447ab52e')
-    ants = AntiFraud.objects.filter(id=course)
-    print(f'时间：{ants[0].datatime}')
-    ant = ants[0].status
-    article = json.dumps({'status': ant, 'errorcode': 100}, ensure_ascii=False)
+    # 根据数据库id字段查询数据返回数据列表
+    ants = AntiFraud.objects.filter(id=course)[0]
+    ant = ants.status  # 账号状态
+
+    # 账号状态为False：已过期直接返回数据
+    if not eval(ant):
+        article = json.dumps({
+            'status': ant,
+            'context': '代理到期请及时续费!',
+            'errorcode': 10002},
+            ensure_ascii=False)
+        print('代理到期请及时续费!')
+        return HttpResponse(article, content_type='application/json')
+
+    # 账号已过有效期更改数据库账号状态
+    lasting = ants.udeta  # 更新时间字段数据
+    print(lasting)
+    if not proxy(lasting):
+        # 根据id条件 修改数据库status字段
+        AntiFraud.objects.filter(id=course).update(status=False)
+        print('账号状态修改成功!')
+
+    # 构造返回json数据
+    ants = AntiFraud.objects.filter(id=course)[0]
+    ant = ants.status  # 账号状态
+    article = json.dumps({
+        'status': ant,
+        'context': '账号状态正常!',
+        'errorcode': 10001},
+        ensure_ascii=False)
     return HttpResponse(article, content_type='application/json')
 
 
-    """返回json数据"""
-    # resp = {'errorcode': 100, 'detail': 'Get success'}
-    # return HttpResponse(json.dumps(resp), content_type='application/json')
+def proxy(lasting):
+    """效验账号有效期"""
+    print(lasting, type(lasting))
+    # lasting 数据库中时间加30天
+    lasting = lasting + + datetime.timedelta(days=30)
+    # 时间类型转换为字符串
+    lasting = lasting.strftime('%Y-%m-%d %H:%M:%S')
+    print(lasting, type(lasting))
+    # 字符串转为时间数组
+    timeArray = time.strptime(lasting, "%Y-%m-%d %H:%M:%S")
+    # timeArray可以调用tm_year等
+    # print(timeArray.tm_year)
+    # 时间数组转为时间戳 秒级
+    timeStamp = int(time.mktime(timeArray))
+    print(timeStamp)
 
-    # article_info = {}
-    # data = json.loads(json.dumps(article_info))
-    # data['article1'] = 'NONE'
-    # article2 = {'title': 'python基础', 'publish_time': '2019-4-1', 'writer': {}}
-    # data['article2'] = article2
-    # writer = {'name': '李先生', 'sex': '男', 'email': 'xxx@gmail.com'}
-    # data['article2']['writer'] = writer
-    # article = json.dumps(data, ensure_ascii=False)
-    # print(article)
-    # return HttpResponse(article, content_type='application/json')
+    # 当前时间戳
+    now_time = int(round(time.time()))
+    print(now_time)
+    # 当前时间大于账号有效期(更新时间+有效期) 证明账号已过期
+    if now_time > timeStamp:
+        print('代理到期请及时续费!')
+        return False
+    return True
