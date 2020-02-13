640792f070f8d82eff18a0851018a476e9b33f18
Administrator <1033383881@qq.com>
2020-02-13 10:43:44 +0800

    转账码

--- 小爬虫/转账码生成/银行卡转账码/spider.py
+++ 小爬虫/转账码生成/银行卡转账码/spider.py
@@ -171,21 +171,21 @@ class Spider:
     def run(self):
         # 隐藏卡号url
         # cardID 隐藏卡号 获取方法  https://zhuanlan.zhihu.com/p/65495172
-        content = ['622908***8318', 'CIB', '-测试', '兴业银行 ', '2001101664858915777']
+        content = ['621790***7893', 'BOC', '滕用开', '兴业银行 ', '2002131746443785873']
         _url = f'https://www.alipay.com/?appId=09999988&actionType=toCard&sourceId=bill&cardNo={content[0]}&' \
                f'bankAccount={content[2]}&money=&amount=&bankMark={content[1]}&bankName={content[3]}&cardIndex={content[4]}&cardNoHidden=true&cardChannel=HISTORY_CARD&orderSource=from'
         print(_url)
 
         # 普通转账url
         # content = self.read_txt()
-        # 新接口
+        # # 新接口
         # _url = f'https://ds.alipay.com/?from=mobilecodec&scheme=alipays%3A%2F%2Fplatformapi%2Fstartapp%3FappId%3D20000200' \
         #        f'%26actionType%3DtoCard%26sourceId=bill%26cardNo={content[1]}%26bankAccount={content[0]}%0A%26money=' \
         #        f'%26amount=%26bankMark={content[3]}%26bankName={content[4]}%26cardNoHidden=true%26cardChannel=HISTORY_CARD%26orderSource=from'
         # 旧接口
         # _url = 'https://www.alipay.com/?appId=09999988&actionType=toCard&sourceId=bill&cardNo={}&bankAccount={}&' \
         #        'money=&amount=&bankMark={}&bankName={}&orderSource='.format(content[1], content[0], content[3], content[4])
-        # print(_url)
+        print(_url)
 
         # 防风控制作
         # _url = quote(_url, string.printable)
