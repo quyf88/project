72676c63d20c80a754f2bea97f33145af9e7d9d9
Administrator <1033383881@qq.com>
2020-03-28 20:45:04 +0800

    抖音

--- 抖音/热门评论数据/多开.py
+++ 抖音/热门评论数据/多开.py
@@ -3,7 +3,7 @@
 # IED ：PyCharm
 # 时间 ：2019/10/31 0031 13:25
 # 版本 ：V1.3
-# 抖音版本 ：10.0.0
+# 抖音版本 ：10.4.0
 import os
 import time
 import psutil
@@ -47,8 +47,8 @@ class Spider:
         # self.x = self.driver.get_window_size()['width']  # 宽
         # self.y = self.driver.get_window_size()['height']  # 长
         # print(self.x, self.y)
-        self.one = 'com.ss.android.ugc.aweme:id/a52'  # 评论数量ID
-        self.two = 'com.ss.android.ugc.aweme:id/dlr'  # 评论数据模块ID
+        self.one = 'com.ss.android.ugc.aweme:id/a6j'  # 评论数量ID
+        self.two = 'com.ss.android.ugc.aweme:id/dt5'  # 评论数据模块ID
 
     def slide(self):
         """
@@ -105,7 +105,7 @@ class Spider:
                         try:
                             con = WebDriverWait(self.driver, 5, 1, AttributeError).until(
                                 EC.presence_of_all_elements_located((By.XPATH,
-                                                                     '//android.support.v7.widget.RecyclerView [@resource-id="com.ss.android.ugc.aweme:id/dlr"]/android.widget.FrameLayout/android.widget.TextView')))
+                                                                     '//android.support.v7.widget.RecyclerView [@resource-id="com.ss.android.ugc.aweme:id/dt5"]/android.widget.FrameLayout/android.widget.TextView')))
                             # print(con, len(con))
                             if con:
                                 print('到达底部,切换下一个视频')
