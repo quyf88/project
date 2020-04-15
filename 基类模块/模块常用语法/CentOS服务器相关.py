d74d21e50a476f9b5586c35b337c5fc78506b22e
Administrator <1033383881@qq.com>
2020-04-15 16:35:05 +0800

    cnetos 命令

--- 基类模块/模块常用语法/CentOS服务器相关.py
+++ 基类模块/模块常用语法/CentOS服务器相关.py
@@ -6,9 +6,9 @@
 
 """nohup 后台不间断运行程序"""
 # 2>&1:错误和输出都传到nohup.out文件中;&:后台运行
-# nohup python spider.py 2>&1 &
+# nohup python 批量添加子站.py 2>&1 &
 # >./log.txt:错误和输出都传到log.txt文件中
-# nohup python spider.py >./log.txt  2>&1 &
+# nohup python 批量添加子站.py >./log.txt  2>&1 &
 # ps -ef | grep python  查看python进程
 
 
